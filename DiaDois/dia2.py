ee9f5ca6d986329e3ef1ea8cacffe03140f02804
RobsonJun10r <robson.alves.junior2@gmail.com>
2024-11-11 13:35:47 -0300

    Alteração Pasta

--- DiaDois/dia2.py
+++ DiaDois/dia2.py
@@ -4,22 +4,24 @@ altura = 1.80
 estudante = True
 
 # Números inteiros = são números sem casas decimais, positivos ou negativos incluindo zero
+
 idade1 = 30
 quantidade = -5
 ano = 2021
 
 # Números com ponto flutuante(Float) = São npumeros com casas decimais, também conhecidos como números reais.
+
 altura1 = 1.75
 peso = 68.5
 temperatura = -3.4
 
 # Cadeia de caracteres(String) = São sequências de caracteres(letras, números, símbolos) São representadas por '' ou ""
+
 name = "João"
 cidade = 'São Paulo'
 mensagem = "Olá Mundo"
 
 # Booleans = São valores lógicos que representam Verdadeiro(True) ou Falso(False)
-
 
 print("Nome:", nome)
 print("Idade:", idade)
@@ -35,6 +37,7 @@ frase = "Olá meu nome é" + nome + " e eu tenho " + str(idade) + " anos." #str(
 print(frase)
 
 # Calculadora simples
+
 numero1 = float(input("Digite o primeiro número:"))
 numero2 = float(input("Digite o segundo número:"))
 
@@ -55,6 +58,7 @@ fahrenheit = celsius * 9/5 + 32
 print("A temperatura em fahrenheit é: ", fahrenheit)
 
 # Área de um circulo
+
 raio = float(input("Qual o raio do círculo?"))
 pi = 3.14159
 area = pi * raio**2
